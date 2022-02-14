e7fc6d9ef221555bc68a0c16d223eafa816d2aae
ejmin <vvejmin@gmail.com>
2022-02-14 18:01:57 +0400

    Initial commit

--- HW2/Practical_Homework_2/factory_design_pattern_2.py
+++ HW2/Practical_Homework_2/factory_design_pattern_2.py
@@ -1,1 +1,9 @@
 # 1
+class BankAccount:
+      def __init__(self, id, balance, rate):
+          self.id = id
+          self.balance = balance
+          self.rate = rate
+
+bank_account = BankAccount(2,6,5)
+
