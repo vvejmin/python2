b1f70f12654b46b1503b49005ad2a782f4b2311b
ejmin <vvejmin@gmail.com>
2022-02-28 18:36:54 +0400

    Merge remote-tracking branch 'origin/master'

--- HW3/practical_homework_3.py
+++ HW3/practical_homework_3.py
@@ -11,18 +11,36 @@ class Target(metaclass=abc.ABCMeta):
         def words(self):
                 pass
 
+        @abc.abstractmethod
+        def translator(self):
+                pass
 
 class Adapter(Target):
         def words(self):
                 print(self.adaptee.specific_words())
-
+        def translator(self):
+                print(self.adaptee.specific_translated_words())
 
 class Adaptee:
 
         def specific_words(self):
                 return ['girasol', 'pájaro']
 
+        def specific_translated_words(self):
+                return {'sunflower':'girasol', 'bird':'pájaro'}
 
 adapter = Adapter()
 adapter.words()
+adapter.translator()
+
 # 2
+class Singleton:
+        __shared_state = {'user_1': 'free', 'user_2': 'busy', 'user_3': 'free'}
+
+        def __init__(self):
+                self.__dict__ = self.__shared_state
+
+        def busy_to_free(self):
+                if Singleton.__shared_state.keys('user_2') == 'busy':
+                        Singleton()
+                return Singleton.__shared_state
