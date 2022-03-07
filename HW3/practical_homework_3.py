b6a6434fd0b86bb96d425c973a2f25e50a2637e7
ejmin <vvejmin@gmail.com>
2022-03-07 18:25:43 +0400

    Merge remote-tracking branch 'origin/master'

--- HW3/practical_homework_3.py
+++ HW3/practical_homework_3.py
@@ -38,6 +38,35 @@ adapter.words()
 adapter.translator()
 
 
+# 2
+import numpy as np
+from abc import ABCMeta,abstractmethod
+
+class Video:
+
+        def play(self):
+                pass
+
+class Media:
+        pass
+
+class MediatoVideo:
+        pass
+
+#Client
+class Musician:
+
+        def __init__(self, media, adapter, player):
+                self.media = media
+                self.adapter = adapter
+                self.player = player
+
+        def play(self):
+                self.media = self.adapter.convert_file()
+                if (self.media.hasAudio == self.player.supportsAudio):
+                        pass
+
+
 # 3
 class Singleton:
         __shared_state = dict()
