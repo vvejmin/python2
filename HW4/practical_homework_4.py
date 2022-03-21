aecdd75e39424b98919e57a46f67958ff6de3fcc
ejmin <vvejmin@gmail.com>
2022-03-21 14:54:20 +0400

    Merge remote-tracking branch 'origin/master'

--- HW4/practical_homework_4.py
+++ HW4/practical_homework_4.py
@@ -4,75 +4,94 @@ from abc import ABC, abstractmethod
 class MessageSender(ABC):
 
         @abstractmethod
-        def send(self, texts, imgs):
+        def send(self, texts):
                pass
 
 class EmailSender(MessageSender):
-    def send(self, texts, imgs):
-       print(f'the email is {texts} {imgs}')
+
+    def send(self, text):
+       print(f'Sending Email: {text}')
 
 
 class SMSSender(MessageSender):
-    def send(self, texts):
-            print(f'the email is {texts}')
+    def send(self, text):
+            print(f'Sending SMS: {text}')
 
 class Client:
-        def __init__(self, sender):
-                self.sender = sender
 
-        def send_message(self, text):
-            self.sender.send.text
+    def __init__(self, sender):
+        self.sender = sender
+
+    def send_message(self, text):
+        self.sender.send(text)
 
 client1 = Client(EmailSender())
 client1.send_message('Hello')
 
 
-client2 = Client(EmailSender())
+client2 = Client(SMSSender())
 client2.send_message('Hello')
 
 # problem 2
 class Instrument:
 
-        def __init__(self, functionality):
-                self.functionality = functionality
+    def __init__(self, functionality):
+        self.functionality = functionality
 
-        def make(self):
-                self.
+    def make(self):
+        print('\t', end= '')
+        print(self.functionality)
+
+
 class InstrumentCollection:
-        def __init__(self, functionality):
-                self.functionality = self.instrument_collections = []
+
+    def __init__(self, collection_id):
+        self.collection_id = collection_id
+        self.instruments = []
+
+    def add(self, instrument):
+            self.instruments.append(instrument)
+
+    def remove(self):
+        self.instruments.remove()
+
+    def make(self):
+        print(self.collection_id)
+
+        for instrument in self.instruments:
+                print('\t', end = '')
+                instrument.make()
+
+class Box:
+
+      def __init__(self, functionality):
+              self.functionality = functionality
+              self.instrument_collections = []
+
+      def make(self):
+          print(self.functionality)
+
+          for instrument_c in self.instrument_collections:
+              print('\t', end = '')
+              instrument_c.make()
+
+      def add(self, instrument_c):
+              self.instrument_collections.append(instrument_c)
+
+      def remove(self, instrument_c):
+              self.instrument_collections.remove(instrument_c)
 
 box = Box('Box')
+col1 = InstrumentCollection('Collection 1')
+col2 = InstrumentCollection('Collection 2')
+pen = Instrument('Pen')
+pencil = Instrument('Pencil')
+rubber = Instrument('Rubber')
 
+col1.add(pen)
+col1.add(pencil)
 
+box.add(col1)
+box.add(col2)
 
-
-
-class Box(InstrumentCollection):
-        def __init__(self, instruments: list):
-                self._instruments = instruments
-
-        def Make(self):
-                pass
-
-        def Remove(instr):
-                pass
-
-        def GetChild(self):
-                pass
-
-class Pen(InstrumentCollection):
-        def Make(self):
-             pass
-
-class Pencil(InstrumentCollection):
-        def Make(self):
-                pass
-
-class Rubber(InstrumentCollection):
-        def Make(self):
-                pass
-
-
-
-
+box.make()
--- HW5/practical_homework_5.py
+++ HW5/practical_homework_5.py
@@ -1,81 +1,105 @@
 #1
-def person(func):
-        def wrapper(*args, **kwargs):
-                value = func(*args, **kwargs)
+def and_wrapper(func):
+    def wrapper(*args, **kwargs):
+        value = func(*args, **kwargs)
 
-                return f'&&& {value} &&&'
-        return wrapper
+        return f'&&& {value} &&&'
+    return wrapper
 
-@person
 def asteriks_wrapper(func):
-        pass
+    def wrapper(*args, **kwargs):
+        value = func(*args, **kwargs)
+
+        return f'*** {value} ***'
+    return wrapper
 
 class Person:
 
-        def __init__(self, name, last_name, age, email):
-                self.name = name
-                self.last_name = last_name
-                self.age = age
-                self.email = email
+        def __init__(self, first_name, last_name, email, age):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.email = email
+            self.age = age
 
-        def print_info(self):
-                pass
+        def print_information(self):
+            if self.age < 14:
+                @asteriks_wrapper
+                @and_wrapper
+                def printer():
+                    return self.__base_printer()
 
-person = Person()
+            else:
+                @and_wrapper
+                def printer():
+                    return self.__base_printer()
+            return printer()
 
-
-
-# 2
-import time
-
-def time_to_sleep(func):
-        def wrapper():
-                begin = time.time()
-
-                func()
-
-                end = time.time()
-                print("total time taken in :", func.__name__, end - begin)
-        return wrapper
-
-@time_to_sleep
-def text():
-    print("Happy pi day!")
-    time.sleep(2)
-    return None
-
-print(text())
+        def __base_printer(self):
+            return f'{self.first_name} - {self.last_name} - {self.email}'
 
 
-# 3
-class Dispatcher:
-        def dispatch_order(self):
-            print("what do you want?")
+person1 = Person('A', 'B', 'a@gmail.com', 12)
+print(person1.print_information())
+
+person2 = Person('A', 'B', 'b@gmail.com', 15)
+print(person2.print_information())
 
 
-class Resturant:
-
-        def cook_food(self):
-                print("the food is ready!")
 
 
-class DeliveryGuy:
-        def delivered_food(self):
-                print("the food is delivered!")
-
-
-class Food:
-
-        def __init__(self):
-                self.dispatcher = Dispatcher()
-                self.resturant = Resturant()
-                self.delivery_guy = DeliveryGuy()
-
-        def organize_priority(self):
-                self.dispatcher.dispatch_order()
-                self.resturant.cook_food()
-                self.delivery_guy.delivered_food()
-
-
-food = Food()
-food.organize_priority()
+#
+#
+# # 2
+# import time
+#
+# def time_to_sleep(func):
+#         def wrapper():
+#                 begin = time.time()
+#
+#                 func()
+#
+#                 end = time.time()
+#                 print("total time taken in :", func.__name__, end - begin)
+#         return wrapper
+#
+# @time_to_sleep
+# def text():
+#     print("Happy pi day!")
+#     time.sleep(2)
+#     return None
+#
+# print(text())
+#
+#
+# # 3
+# class Dispatcher:
+#         def dispatch_order(self):
+#             print("what do you want?")
+#
+#
+# class Resturant:
+#
+#         def cook_food(self):
+#                 print("the food is ready!")
+#
+#
+# class DeliveryGuy:
+#         def delivered_food(self):
+#                 print("the food is delivered!")
+#
+#
+# class Food:
+#
+#         def __init__(self):
+#                 self.dispatcher = Dispatcher()
+#                 self.resturant = Resturant()
+#                 self.delivery_guy = DeliveryGuy()
+#
+#         def organize_priority(self):
+#                 self.dispatcher.dispatch_order()
+#                 self.resturant.cook_food()
+#                 self.delivery_guy.delivered_food()
+#
+#
+# food = Food()
+# food.organize_priority()
