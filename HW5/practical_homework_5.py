43926e45ab4b38e426e3587c72d866ab56d24892
ejmin <vvejmin@gmail.com>
2022-03-21 14:55:03 +0400

    Merge remote-tracking branch 'origin/master'

--- HW5/practical_homework_5.py
+++ HW5/practical_homework_5.py
@@ -47,59 +47,57 @@ print(person2.print_information())
 
 
 
-#
-#
-# # 2
-# import time
-#
-# def time_to_sleep(func):
-#         def wrapper():
-#                 begin = time.time()
-#
-#                 func()
-#
-#                 end = time.time()
-#                 print("total time taken in :", func.__name__, end - begin)
-#         return wrapper
-#
-# @time_to_sleep
-# def text():
-#     print("Happy pi day!")
-#     time.sleep(2)
-#     return None
-#
-# print(text())
-#
-#
-# # 3
-# class Dispatcher:
-#         def dispatch_order(self):
-#             print("what do you want?")
-#
-#
-# class Resturant:
-#
-#         def cook_food(self):
-#                 print("the food is ready!")
-#
-#
-# class DeliveryGuy:
-#         def delivered_food(self):
-#                 print("the food is delivered!")
-#
-#
-# class Food:
-#
-#         def __init__(self):
-#                 self.dispatcher = Dispatcher()
-#                 self.resturant = Resturant()
-#                 self.delivery_guy = DeliveryGuy()
-#
-#         def organize_priority(self):
-#                 self.dispatcher.dispatch_order()
-#                 self.resturant.cook_food()
-#                 self.delivery_guy.delivered_food()
-#
-#
-# food = Food()
-# food.organize_priority()
+# 2
+import time
+
+def time_to_sleep(func):
+        def wrapper():
+                begin = time.time()
+
+                func()
+
+                end = time.time()
+                print("total time taken in :", func.__name__, end - begin)
+        return wrapper
+
+@time_to_sleep
+def text():
+    print("Happy pi day!")
+    time.sleep(2)
+    return None
+
+print(text())
+
+
+# 3
+class Dispatcher:
+        def dispatch_order(self):
+            print("what do you want?")
+
+
+class Resturant:
+
+        def cook_food(self):
+                print("the food is ready!")
+
+
+class DeliveryGuy:
+        def delivered_food(self):
+                print("the food is delivered!")
+
+
+class Food:
+
+        def __init__(self):
+                self.dispatcher = Dispatcher()
+                self.resturant = Resturant()
+                self.delivery_guy = DeliveryGuy()
+
+        def organize_priority(self):
+                self.dispatcher.dispatch_order()
+                self.resturant.cook_food()
+                self.delivery_guy.delivered_food()
+
+
+food = Food()
+food.organize_priority()
